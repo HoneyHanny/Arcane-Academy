bbe403f61ab791b8d9e5409042dd6a32678d43cc
HoneyHanny <hansduran25@gmail.com>
2024-05-25 12:20:10 +0800

    Add glow

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,14 +1,19 @@
 import { ThemeToggle } from './ThemeToggle'
 import { Button } from './ui/button'
 import { Link } from 'react-router-dom'
-import logo from '../../public/arcane.svg'
+import logo from '/arcane.svg'
+import '../styles/appbar.css'
 
 const Appbar = () => {
   return (
-    <header className="py-4 md:py-8 border-b px-2 md:px-8 lg:px-16 flex flex-row justify-between">
-      <div className="flex gap-4">
-        <img src={logo} alt="arcane academy" className="h-12 w-12" />
-        <h1 className="scroll-m-20 text-2xl font-bold tracking-tight md:text-4xl lg:text-5xl">
+    <header className="py-2 px-2 lg:px-8 border-b flex flex-row justify-between">
+      <div className="flex gap-4 items-center">
+        <img
+          src={logo}
+          alt="Arcane Academy"
+          className="h-10 w-10 md:h-16 md:w-16 lg:h-24 lg:w-24 logo"
+        />
+        <h1 className="scroll-m-20 text-1xl font-bold tracking-tight md:text-4xl lg:text-5xl">
           Arcane Academy
         </h1>
       </div>
