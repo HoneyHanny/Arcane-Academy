d3932f34d577afedf728f4ba99d83a542cdfcd1c
HoneyHanny <hansduran25@gmail.com>
2024-06-09 21:05:34 +0800

    Add github button

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -6,21 +6,20 @@ import '../styles/appbar.css'
 
 const Appbar = () => {
   return (
-    <header className="py-2 px-2 lg:px-8 border-b flex flex-row justify-between">
-      <div className="flex gap-4 items-center">
-        <img
-          src={logo}
-          alt="Arcane Academy"
-          className="h-10 w-10 md:h-16 md:w-16 lg:h-24 lg:w-24 logo"
-        />
-        <h1 className="scroll-m-20 text-1xl font-bold tracking-tight md:text-4xl lg:text-5xl">
-          Arcane Academy
-        </h1>
+    <header className='py-2 px-2 lg:px-8 border-b flex flex-row justify-between'>
+      <div className='flex gap-4 items-center'>
+        <img src={logo} alt='Arcane Academy' className='h-10 w-10 md:h-16 md:w-16 lg:h-24 lg:w-24 logo' />
+        <h1 className='scroll-m-20 text-1xl font-bold tracking-tight md:text-4xl lg:text-5xl'>Arcane Academy</h1>
       </div>
-      <div className="flex items-center space-x-2 lg:gap-16">
-        <Link to="/home">
-          <Button variant="outline">Home</Button>
-        </Link>
+      <div className='flex items-center space-x-2 lg:gap-16'>
+        <div className='flex items-center space-x-2 lg:gap-2'>
+          <Link to='/home'>
+            <Button variant='outline'>Home</Button>
+          </Link>
+          <Link to='https://github.com/HoneyHanny/Arcane-Academy'>
+            <Button variant='outline'>Github</Button>
+          </Link>
+        </div>
         <ThemeToggle />
       </div>
     </header>
